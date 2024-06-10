e98cb04e41cf197e9793c5f7fbf24738ed7ca819
Peter Shao <shaoruitai@gmail.com>
2024-06-09 21:39:38 -0400

    .

---
+++ chess/experimental_ensemble_2-backup.R
@@ -0,0 +1,44 @@
+# Showing the proportion of white wins holds constant
+# for any set of games (assuming skill is equally distributed among white and black players)
+
+tuning_results_2_2 <- data.frame(guess_white_accuracy = numeric(),
+                             dataset_size = numeric(),
+                             cutoff = numeric(),
+                             stringsAsFactors = FALSE)
+
+# Plot accuracy of "white wins" against the same cutoffs as "higher rated wins"
+for (cutoff in seq(from = max(abs(main_df$white_rating - main_df$black_rating)), to = 0, by = -1)) {
+  filtered <- main_df %>%
+    filter(abs(white_rating - black_rating) <= cutoff)
+  guess_white_accuracy <- calculate_accuracy(rep(1, nrow(filtered)), filtered$winner)
+  tuning_results_2_2 <- rbind(tuning_results_2_2, data.frame(
+    guess_white_accuracy = guess_white_accuracy,
+    dataset_size = nrow(filtered),
+    cutoff = cutoff))
+}
+
+rm(cutoff, accuracy, filtered, predicted)
+
+# Comparison to Global White WR
+by_rating_acc <- calculate_accuracy(
+  ifelse(main_df$white_rating >= main_df$black_rating, 1, 0),
+  main_df$winner)
+
+# Subsetting has negligible effect on white's win rate:
+plot <- ggplot(tuning_results_2_2, aes(x = cutoff)) +
+  geom_hline(yintercept = by_majority_acc, linetype = "dashed", color = "red") +
+  geom_line(aes(y = guess_white_accuracy, color = "Predict White Wins")) +
+  scale_color_manual(values = c("Predict White Wins" = "red"), guide = "none") +
+  scale_x_reverse() +
+  labs(x = "Maximum Rating Advantage", y = "") +
+  ggtitle("Max Rating Advantage Subsetting") +
+  theme_minimal()+
+  theme(
+    text = element_text(size = unit(10, "mm")),
+    plot.title = element_text(size = unit(20, "mm")),
+    axis.title = element_text(size = unit(15, "mm")),
+    axis.text = element_text(size = unit(10, "mm"))
+  )
+
+print(plot)
+#store_plot("cutoff_subsetting2-white_wins.png", plot2)
--- chess/experimental_ensemble_2.R
+++ chess/experimental_ensemble_2.R
@@ -8,10 +8,7 @@
 # (Note this assumes the proportion of white wins holds constant
 # for any set of games where skill is equally distributed among white and black players)
 
-
-
-tuning_results_2 <- data.frame(higher_rating_accuracy = numeric(),
-                               guess_white_accuracy = numeric(),
+tuning_results_2 <- data.frame(accuracy = numeric(),
                              dataset_size = numeric(),
                              cutoff = numeric(),
                              stringsAsFactors = FALSE)
@@ -22,12 +19,10 @@ for (cutoff in seq(from = max(abs(main_df$white_rating - main_df$black_rating)),
   filtered <- main_df %>%
     # Cutoff now defines MAXIMUM rating diff, not minimum
     filter(abs(white_rating - black_rating) <= cutoff)
-  higher_rating_predictions <- ifelse(filtered$white_rating >= filtered$black_rating, 1, 0)
-  higher_rating_accuracy <- calculate_accuracy(higher_rating_predictions, filtered$winner)
-  guess_white_accuracy <- calculate_accuracy(rep(1, nrow(filtered)), filtered$winner)
+  predictions <- ifelse(filtered$white_rating >= filtered$black_rating, 1, 0)
+  accuracy <- calculate_accuracy(predictions, filtered$winner)
   tuning_results_2 <- rbind(tuning_results_2, data.frame(
-    higher_rating_accuracy = higher_rating_accuracy,
-    guess_white_accuracy = guess_white_accuracy,
+    accuracy = accuracy,
     dataset_size = nrow(filtered),
     cutoff = cutoff))
 }
@@ -42,15 +37,16 @@ by_rating_acc <- calculate_accuracy(
   ifelse(main_df$white_rating >= main_df$black_rating, 1, 0),
   main_df$winner)
 
+# First cutoff where higher player wins gives lower accuracy than guessing white for all
+threshold <- max(tuning_results_2[which(tuning_results_2$accuracy < by_majority_acc),]$cutoff)
+
 plot <- ggplot(tuning_results_2, aes(x = cutoff)) +
+  scale_x_reverse() +
+  geom_rect(aes(xmin = -Inf, xmax = threshold, ymin = -Inf, ymax = Inf), fill = "lightcoral", alpha = 0.2) +
   geom_hline(yintercept = by_majority_acc, linetype = "dashed", color = "red") +
   geom_hline(yintercept = by_rating_acc, linetype = "dashed", color = "blue") +
-  #geom_point(aes(y = accuracy, color = "Accuracy")) +
-  geom_line(aes(y = higher_rating_accuracy, color = "Predict Higher Rated Wins"), size = 1.5) +
-  #geom_line(aes(y = guess_white_accuracy, color = "Predict White Wins")) +
-  #scale_color_manual(values = c("Predict Higher Rated Wins" = "blue", "Predict White Wins" = "red")) +
+  geom_line(aes(y = accuracy, color = "Predict Higher Rated Wins"), size = 1.5) +
   scale_color_manual(values = c("Predict Higher Rated Wins" = "blue"), guide = "none") +
-  scale_x_reverse() +
   labs(x = "Maximum Rating Advantage", y = "") +
   ggtitle("Max Rating Advantage Subsetting") +
   theme(legend.position = "right")+
@@ -64,27 +60,3 @@ plot <- ggplot(tuning_results_2, aes(x = cutoff)) +
 
 print(plot)
 store_plot("cutoff_subsetting2.png", plot)
-
-# Subsetting has negligible effect on white's win rate:
-#plot2 <- ggplot(tuning_results_2, aes(x = cutoff)) +
-#  geom_hline(yintercept = by_majority_acc, linetype = "dashed", color = "red") +
-#  geom_hline(yintercept = by_rating_acc, linetype = "dashed", color = "blue") +
-#  #geom_point(aes(y = accuracy, color = "Accuracy")) +
-#  #geom_line(aes(y = higher_rating_accuracy, color = "Predict Higher Rated Wins"), size = 1.5) +
-#  geom_line(aes(y = guess_white_accuracy, color = "Predict White Wins")) +
-#  #scale_color_manual(values = c("Predict Higher Rated Wins" = "blue", "Predict White Wins" = "red")) +
-#  scale_color_manual(values = c("Predict White Wins" = "red")) +
-#  scale_x_reverse() +
-#  labs(x = "Maximum Rating Advantage", y = "") +
-#  ggtitle("Max Rating Advantage Subsetting") +
-#  theme(legend.position = "right")+
-#  theme_minimal()+
-#  theme(
-#    text = element_text(size = unit(10, "mm")),
-#    plot.title = element_text(size = unit(20, "mm")),
-#    axis.title = element_text(size = unit(15, "mm")),
-#    axis.text = element_text(size = unit(10, "mm"))
-#  )
-
-#print(plot2)
-#store_plot("cutoff_subsetting2-white_wins.png", plot2)
