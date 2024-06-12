c19dfbef8bacb2b4d7ac9b236d2d09e46bd776d5
Peter Shao <shaoruitai@gmail.com>
2024-06-11 22:56:55 -0400

    .

--- chess/3 - ratings_exploration.R
+++ chess/3 - ratings_exploration.R
@@ -1,3 +1,25 @@
+# White vs. Black Rating
+plot <- ggplot(main_df, aes(x = white_rating, y = black_rating, color = factor(winner))) +
+  geom_point() +
+  scale_color_manual(values = c("blue", "red"), labels = c("Black", "White")) +
+  labs(title = "White Vs. Black Ratings ",
+       x = "White's Rating",
+       y = "Black's Rating",
+       color = "Winner") +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", size=1.5, color = "green") +
+  theme_minimal()+
+  theme(
+    text = element_text(size = unit(10, "mm")),
+    plot.title = element_text(size = unit(20, "mm")),
+    axis.title = element_text(size = unit(15, "mm")),
+    axis.text = element_text(size = unit(10, "mm"))
+  )
+print(plot)
+store_plot("white_vs_black_ratings.png", plot, h = 6, w=6)
+rm(plot)
+
+#----------------------
+# White's WR vs. Rating Advantage
 plot_df <- main_df %>%
   mutate(rating_diff = white_rating - black_rating) %>%
   select(rating_diff, winner) %>%
